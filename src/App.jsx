708e1493a3a57c9f362324246344e64fa97f2f0e
Aadya Singh <aadyas72@gmail.com>
2025-03-11 15:59:51 +0530

    Contact

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
 import Tech from './components/Tech';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 const App = () => {
   return (
@@ -22,6 +24,8 @@ const App = () => {
         <Hero />
         <About/>
         <Tech/>
+        <Projects/>
+        <Contact/>
       </div>
 
     </div>
