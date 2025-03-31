88bc559910167cdb5476d87ff04efe7aff4a8add
Aadya Singh <aadyas72@gmail.com>
2025-03-31 14:51:47 +0530

    minor changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -15,36 +15,7 @@ Beyond tech, I’m a visual storyteller. Being part of Adlib Arts (Photography &
 
 When I’m not designing, editing, or geeking out over new tech, you’ll find me chasing sunsets, planning my next trip, or binging horror and thriller movies. And my music? A perfect mix of Taylor Swift’s poetic brilliance and The Weeknd’s dark, moody vibes.`;
 
-export const EXPERIENCES = [
-  {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
-  },
-  {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  },
-];
+
 
 export const PROJECTS = [
   {
@@ -65,15 +36,15 @@ export const PROJECTS = [
     title: "Portfolio Website",
     image: project3,
     description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
+      "A responsive and visually appealing portfolio website using Vite, Tailwind CSS, and React . Animations added with the help of Framer Motion. Showcases projects and skills with optimized performance and modern UI/UX principles. Hosted on Vercel for optimized performance.",
     technologies: ["React", "TailwindCSS", "Framer Motion"],
   },
   {
     title: "AI-LinkedIn Post Generator Tool",
     image: project4,
     description:
-      "A powerful AI-driven tool built using LangChain, Streamlit, and Groq Cloud to generate engaging LinkedIn posts effortlessly. It leverages LangChain for structuring LLM interactions, Streamlit for an intuitive UI, and Groq Cloud for ultra-fast AI responses.",
-    technologies: ["LLM", "Langchain", "Streamlit", "Groq cloud", "Python"],
+      "A powerful AI-driven tool built using LangChain, Streamlit, and Groq Cloud to generate engaging LinkedIn posts effortlessly. It leverages LangChain for structuring LLM interactions, Streamlit for an intuitive UI, and Groq Cloud for ultrafast AI responses. The project is fully Dockerized, ensuring seamless deployment and scalability.",
+    technologies: ["LLM", "Langchain", "Streamlit", "Groq cloud", "Python","Docker"],
   },
 ];
 
